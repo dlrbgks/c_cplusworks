fa0de612be84888f37d3c2bb9c96e92700cf6f65
dlrbgks <atipact65@gmail.com>
2025-07-25 17:23:13 +0900

    소스 파일

---
+++ Basic/cmathEx.cpp
@@ -0,0 +1,31 @@
+//day15-1
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	//반올림 - round()
+	cout << round(1.54) << endl;
+
+	//내림 - floor()
+	cout << floor(3.3) << endl;
+
+	//올림 - ceil()
+	cout << ceil(3.3) << endl;
+
+	//절대값 - abs()
+	cout << abs(-8.8) << endl;
+
+	//거듭제곱 - pow()
+	cout << pow(2, 4) << endl;
+
+	//제곱근 - sqrt()
+	cout << sqrt(9) << endl;
+
+
+
+
+
+
+	return 0;
+}
